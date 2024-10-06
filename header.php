6aefd9b19202fd330df812052148853fe71bfc34
AccppK <167609616+AccppK@users.noreply.github.com>
2024-10-06 19:03:37 +0800

    Update header.php

--- header.php
+++ header.php
@@ -1,7 +1,171 @@
 
+<?php
+    if($_SESSION[$OJ_NAME.'_'.'administrator']==false){
+        ?>
+        <script>
+	//禁止鼠标右击
+    document.oncontextmenu = function() {
+        event.returnValue = false;
+      };
+      let userAgent = navigator.userAgent;
+      if (userAgent.indexOf("Firefox") > -1) {
+        let checkStatus;
+        let devtools = /./;
+        devtools.toString = function() {
+          checkStatus = "on";
+        };
+        setInterval(function() {
+          checkStatus = "off";
+          console.log(devtools);
+          console.log(checkStatus);
+          console.clear();
+          if (checkStatus === "on") {
+            let target = "";
+            try {
+              window.open("about:blank", (target = "_self"));
+            } catch (err) {
+              let a = document.createElement("button");
+              a.onclick = function() {
+                window.open("about:blank", (target = "_self"));
+              };
+              a.click();
+            }
+          }
+        }, 200);
+      } else {
+        //禁用控制台
+        let ConsoleManager = {
+          onOpen: function() {
+            alert("Console is opened");
+          },
+          onClose: function() {
+            alert("Console is closed");
+          },
+          init: function() {
+            let self = this;
+            let x = document.createElement("div");
+            let isOpening = false,
+              isOpened = false;
+            Object.defineProperty(x, "id", {
+              get: function() {
+                if (!isOpening) {
+                  self.onOpen();
+                  isOpening = true;
+                }
+                isOpened = true;
+                return true;
+              }
+            });
+            setInterval(function() {
+              isOpened = false;
+              console.info(x);
+              console.clear();
+              if (!isOpened && isOpening) {
+                self.onClose();
+                isOpening = false;
+              }
+            }, 200);
+          }
+        };
+        ConsoleManager.onOpen = function() {
+          //打开控制台，跳转
+          let target = "";
+          try {
+            window.open("about:blank", (target = "_self"));
+          } catch (err) {
+            let a = document.createElement("button");
+            a.onclick = function() {
+              window.open("about:blank", (target = "_self"));
+            };
+            a.click();
+          }
+        };
+        ConsoleManager.onClose = function() {
+          alert("Console is closed!!!!!");
+        };
+        ConsoleManager.init();
+    }
+        </script>
+    <script>
+    <?php
+    }
 
 
 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AKOJ</title>
+</head>
+
+<body>
+    <?php
+    if($DIANJI){
+        ?>
+        <script type="text/javascript">
+         ! function (e, t, a) {
+            function r() {
+                for (var e = 0; e < s.length; e++) s[e].alpha <= 0 ? (t.body.removeChild(s[e].el), s.splice(e, 1)) : (s[
+                        e].y--, s[e].scale += .004, s[e].alpha -= .013, s[e].el.style.cssText = "left:" + s[e].x +
+                    "px;top:" + s[e].y + "px;opacity:" + s[e].alpha + ";transform:scale(" + s[e].scale + "," + s[e]
+                    .scale + ") rotate(45deg);background:" + s[e].color + ";z-index:99999");
+                requestAnimationFrame(r)
+            }
+
+            function n() {
+                var t = "function" == typeof e.onclick && e.onclick;
+                e.onclick = function (e) {
+                    t && t(), o(e)
+                }
+            }
+
+            function o(e) {
+                var a = t.createElement("div");
+                a.className = "heart", s.push({
+                    el: a,
+                    x: e.clientX - 5,
+                    y: e.clientY - 5,
+                    scale: 1,
+                    alpha: 1,
+                    color: c()
+                }), t.body.appendChild(a)
+            }
+
+            function i(e) {
+                var a = t.createElement("style");
+                a.type = "text/css";
+                try {
+                    a.appendChild(t.createTextNode(e))
+                } catch (t) {
+                    a.styleSheet.cssText = e
+                }
+                t.getElementsByTagName("head")[0].appendChild(a)
+            }
+
+            function c() {
+                return "rgb(" + ~~(255 * Math.random()) + "," + ~~(255 * Math.random()) + "," + ~~(255 * Math
+                    .random()) + ")"
+            }
+            var s = [];
+            e.requestAnimationFrame = e.requestAnimationFrame || e.webkitRequestAnimationFrame || e
+                .mozRequestAnimationFrame || e.oRequestAnimationFrame || e.msRequestAnimationFrame || function (e) {
+                    setTimeout(e, 1e3 / 60)
+                }, i(
+                    ".heart{width: 10px;height: 10px;position: fixed;background: #f00;transform: rotate(45deg);-webkit-transform: rotate(45deg);-moz-transform: rotate(45deg);}.heart:after,.heart:before{content: '';width: inherit;height: inherit;background: inherit;border-radius: 50%;-webkit-border-radius: 50%;-moz-border-radius: 50%;position: fixed;}.heart:after{top: -5px;}.heart:before{left: -5px;}"
+                ), n(), r()
+        }(window, document);<?php
+    }
+    ?>
+    <!-- 网页鼠标点击特效（爱心） -->
+    
+    </script>
+</body>
+
+</html>
 <?php
     if($OJ_OB ){
         if(!$_SESSION[$OJ_NAME.'_'.'administrator']){
