c93a2aeb33690397892fcff4a81d96a6073b9e45
AccppK <167609616+AccppK@users.noreply.github.com>
2025-01-25 21:31:57 +0800

    Update delete_problem.php

--- web/delete_problem.php
+++ web/delete_problem.php
@@ -6,6 +6,9 @@
     //如果需要,请开启db_info.inc.php里的$DELETE_PROBLEM_PAGE.
     //请及时完成备份
     //代码部分.
+    $BEIFEN="mysqldump -u $DB_USER -p$DB_PASS $DB_NAME problem > table_backup.sql";
+    system($BEIFEN);//备份文件
+    echo "成功备份,备份文件为保存该php目录的'table_backup.sql'.开始删除表数据...</br>";
   if($DELETE_PROBLEM_PAGE){
       $conn=new mysqli($DB_HOST,$DB_USER,$DB_PASS,$DB_NAME);//连接数据库,请确保您安装的是HUSTOJ,并且是较新的开源版本.
   $sql="DELETE FROM `problem`;";//删除所有题目数据,注意,此操作有一定的危险性.
