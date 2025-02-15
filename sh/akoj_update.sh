0dc944f769017421e336498f0877b90fcf9e2a43
AccppK <167609616+AccppK@users.noreply.github.com>
2025-02-15 13:24:12 +0800

    Create akoj_update.sh

---
+++ sh/akoj_update.sh
@@ -0,0 +1,12 @@
+echo "Welcome to akoj update for php shell!</br>";
+echo "Statusing...</br>";
+echo "Go to Github,if your network is stuck,Please try adding a proxy or try again later.</br>";
+echo "Update QianDao,think you 少侠Dr!</br>";
+cd /home/judge/src/web/
+git clone https://github.com/ShaoxiaDr/SHAOXIAOJ/blob/%E6%BA%90%E7%A0%81/web/qiandao_update.php
+echo "脚本已完成,文件在web的qiandao_update.php,请查看少侠Dr的留言</br>";
+echo "执行作者为Accpp_K的GitHub分支:https://github.com/AccppK/AKOJ/blob/main/web/delete_problem.php";
+git clone https://github.com/AccppK/AKOJ/blob/main/web/delete_problem.php
+echo "已完成.";
+ip a
+#END.
